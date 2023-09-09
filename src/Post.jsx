faf1233bfd0c8ba51177bbba306f499038a4e0fe
husneara <husnearaa99@gmail.com>
2023-09-09 21:37:37 +0600

    first commit

---
+++ src/Post.jsx
@@ -0,0 +1,12 @@
+import './Post.css'
+export default function Post({ post }) {
+    const {title, body, id, userId} = post;
+    return (
+        <div className="post">
+            <h5>Tittle: {title} </h5>
+            <p> <small>userId: {userId} </small></p>
+            <p><small> postId: {id} </small></p>
+            <p>{body}</p>
+        </div>
+    )
+}
---
+++ src/Posts.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import Post from "./Post"
+
+export default function Posts(){
+    const [posts, setPosts] = useState([])
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => setPosts(data))
+    }, [])
+    return(
+        <div>
+            <h3>Posts: {posts.length}</h3>
+            {
+                posts.map(post => <Post post={post}></Post>)
+            }
+        </div>
+    )
+}
+
+/***
+ * 1. create a state to store data
+ * 2. create use effect with no dependencies
+ * 3. use fetch to load data
+ */
